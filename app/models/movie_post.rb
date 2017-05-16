8b2599b134276835eb609e4dba56a009da1e9856
noobiwankenoobi <ross.hazelett@gmail.com>
2017-05-16 19:05:38 -0400

    Fixed it back

--- app/models/movie_post.rb
+++ app/models/movie_post.rb
@@ -1,9 +1,9 @@
 class MoviePost < ApplicationRecord
-  has_many :sub_movie_posts, class_name: 'MoviePost'
+  has_many :sub_movie_posts, class_name: 'MoviePost',
+                             foreign_key: 'parent_movie_post_id',
+                             dependent: :destroy
 
-  belongs_to :parent_movie_post, class_name: 'MoviePost',
-                                 foreign_key: 'parent_movie_post_id',
-                                 dependent: :destroy
+  belongs_to :parent_movie_post, class_name: 'MoviePost'
 
   belongs_to :movie
   belongs_to :user
