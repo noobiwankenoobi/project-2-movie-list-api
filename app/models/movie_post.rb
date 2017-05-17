1198a577b6afee9e62ae68a844a53ab187d42dc3
noobiwankenoobi <ross.hazelett@gmail.com>
2017-05-16 22:18:14 -0400

    Fixed it all and made it work
    Was getting 422 error unprocessable entity
    Used console.logs(print) to find where it was breaking
    Found it breaking at MoviePost.save (same as from rails console)
    
    Added optional: true, to the MoviePost model
    making it optional for movie_posts to have a parent_movie_post_id
    Received zero help on this debug, used pure intuition
    Nobody knew that you had to specify that a relationship was optional
    
    A defining moment in a young hacker's career
    5/16 10:23pm

--- app/models/movie_post.rb
+++ app/models/movie_post.rb
@@ -3,7 +3,7 @@ class MoviePost < ApplicationRecord
                              foreign_key: 'parent_movie_post_id',
                              dependent: :destroy
 
-  belongs_to :parent_movie_post, class_name: 'MoviePost'
+  belongs_to :parent_movie_post, optional: true, class_name: 'MoviePost'
 
   belongs_to :movie
   belongs_to :user
