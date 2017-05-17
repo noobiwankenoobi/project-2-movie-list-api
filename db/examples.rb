fe2d07a3da6976c7b00d5eb87f378ba0b1d94056
noobiwankenoobi <ross.hazelett@gmail.com>
2017-05-16 23:39:04 -0400

    Added examples
    User 1 (a@a a) and two Movies

--- db/examples.rb
+++ db/examples.rb
@@ -17,3 +17,44 @@
 #                password: 'abc123',
 #                password_confirmation: nil)
 # end
+
+User.create(email: 'a@a',
+            password: 'a',
+            password_confirmation: 'a')
+
+example_movies = [
+  {
+    user_id: 1,
+    title: 'Inception',
+    title_imdb_url: 'http://www.imdb.com/title/tt1375666/',
+    title_rotten_url: 'https://www.rottentomatoes.com/m/inception/',
+    director: 'Christopher Nolan',
+    director_imdb_url: 'http://www.imdb.com/name/nm0634240/',
+    writer: 'Christopher Nolan',
+    writer_imdb_url: 'http://www.imdb.com/name/nm0634240/',
+    cinematographer: 'Wally Pfister',
+    cinematographer_imdb_url: 'http://www.imdb.com/name/nm0002892/',
+    music: 'Hans Zimmer',
+    music_imdb_url: 'http://www.imdb.com/name/nm0001877/',
+    img_url: 'http://cdn.collider.com/wp-content/uploads/Inception-movie-poster-7.jpg'
+  },
+  {
+    user_id: 1,
+    title: 'Aliens',
+    title_imdb_url: 'http://www.imdb.com/title/tt0090605/',
+    title_rotten_url: 'https://www.rottentomatoes.com/m/1000617_aliens',
+    director: 'James Cameron',
+    director_imdb_url: 'http://www.imdb.com/name/nm0000116/',
+    writer: 'James Cameron',
+    writer_imdb_url: 'http://www.imdb.com/name/nm0000116/',
+    cinematographer: 'Adrian Biddle',
+    cinematographer_imdb_url: 'http://www.imdb.com/name/nm0000939/',
+    music: 'James Horner',
+    music_imdb_url: 'http://www.imdb.com/name/nm0000035/',
+    img_url: 'http://www.impawards.com/1986/posters/aliens_ver1_xlg.jpg'
+  }
+]
+
+example_movies.each do |movie|
+  Movie.create(movie)
+end
